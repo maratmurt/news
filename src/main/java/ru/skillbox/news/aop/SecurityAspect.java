a4dfb86e7a0b300c6fa96e64800d93c79d7b771b
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-08-22 12:56:26 +0300

    refactor: extract SecurityAspect

--- src/main/java/ru/skillbox/news/aop/SecurityAspect.java
+++ src/main/java/ru/skillbox/news/aop/SecurityAspect.java
@@ -32,93 +32,78 @@ public class SecurityAspect {
 
     @Before("userGetByIdMethod()")
     public void secureUserGetById(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = userService.getByName(authentication.getName());
+        User currentUser = getCurrentUser();
         List<RoleType> roles = currentUser.getRoles().stream().toList();
         if (roles.size() == 1 && roles.contains(RoleType.ROLE_USER)) {
-            Long requestedUserId = Arrays.stream(joinPoint.getArgs())
-                    .findFirst()
-                    .map(obj -> (Long) obj)
-                    .orElseThrow();
+            Long requestedUserId = getIdFromArgs(joinPoint.getArgs());
 
             if (!requestedUserId.equals(currentUser.getId())) {
-                throw new SecurityException("Access denied");
+                throw new SecurityException("Доступ к данным других пользователей ограничен");
             }
         }
     }
 
     @Before("updateMethod() && inArticles()")
     public void secureArticleUpdateMethod(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Long currentUserId = userService.getByName(authentication.getName()).getId();
-
-        Long articleId = Arrays.stream(joinPoint.getArgs())
-                .filter(obj -> obj instanceof Long)
-                .findFirst()
-                .map(obj -> (Long) obj)
-                .orElseThrow();
+        User currentUser = getCurrentUser();
+        Long articleId = getIdFromArgs(joinPoint.getArgs());
         Long authorId = articleService.getById(articleId).getAuthor().getId();
 
-        if (!authorId.equals(currentUserId)) {
-            throw new SecurityException("Access denied");
+        if (!authorId.equals(currentUser.getId())) {
+            throw new SecurityException("Допускается редактирование только своего контента");
         }
     }
 
     @Before("updateMethod() && inComments()")
     public void secureCommentUpdateMethod(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Long currentUserId = userService.getByName(authentication.getName()).getId();
-
-        Long commentId = Arrays.stream(joinPoint.getArgs())
-                .filter(obj -> obj instanceof Long)
-                .findFirst()
-                .map(obj -> (Long) obj)
-                .orElseThrow();
+        User currentUser = getCurrentUser();
+        Long commentId = getIdFromArgs(joinPoint.getArgs());
         Long authorId = commentService.getById(commentId).getUser().getId();
 
-        if (!authorId.equals(currentUserId)) {
-            throw new SecurityException("Access denied");
+        if (!authorId.equals(currentUser.getId())) {
+            throw new SecurityException("Допускается редактирование только своих комментариев");
         }
     }
 
     @Before("deleteMethod() && inArticles()")
     public void secureArticleDeleteMethod(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = userService.getByName(authentication.getName());
+        User currentUser = getCurrentUser();
         List<RoleType> roles = currentUser.getRoles().stream().toList();
-
         if (roles.size() == 1 && roles.contains(RoleType.ROLE_USER)) {
-            Long articleId = Arrays.stream(joinPoint.getArgs())
-                    .filter(obj -> obj instanceof Long)
-                    .findFirst()
-                    .map(obj -> (Long) obj)
-                    .orElseThrow();
+            Long articleId = getIdFromArgs(joinPoint.getArgs());
             Long authorId = articleService.getById(articleId).getAuthor().getId();
 
             if (!authorId.equals(currentUser.getId())) {
-                throw new SecurityException("Access denied");
+                throw new SecurityException("Допускается удаление только своего контента");
             }
         }
     }
 
     @Before("deleteMethod() && inComments()")
     public void secureCommentDeleteMethod(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = userService.getByName(authentication.getName());
+        User currentUser = getCurrentUser();
         List<RoleType> roles = currentUser.getRoles().stream().toList();
-
         if (roles.size() == 1 && roles.contains(RoleType.ROLE_USER)) {
-            Long commentId = Arrays.stream(joinPoint.getArgs())
-                    .filter(obj -> obj instanceof Long)
-                    .findFirst()
-                    .map(obj -> (Long) obj)
-                    .orElseThrow();
+            Long commentId = getIdFromArgs(joinPoint.getArgs());
             Long authorId = commentService.getById(commentId).getUser().getId();
 
             if (!authorId.equals(currentUser.getId())) {
-                throw new SecurityException("Access denied");
+                throw new SecurityException("Допускается удаление только своих комментариев");
             }
         }
+    }
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userService.getByName(authentication.getName());
+    }
+
+    private Long getIdFromArgs(Object[] args) {
+        return Arrays.stream(args)
+                .filter(obj -> obj instanceof Long)
+                .findFirst()
+                .map(obj -> (Long) obj)
+                .orElseThrow();
     }
 
     @Pointcut("execution(* getById(..))")
